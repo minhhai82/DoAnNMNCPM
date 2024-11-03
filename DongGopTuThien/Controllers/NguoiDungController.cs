db54d84a46641aca4f50f9e97c1e3346148cf418
HuongTr111 <trtrhuong@gmail.com>
2024-11-03 11:40:54 +0700

    Api api/NguoiDung/{id}/submitPaper

--- DongGopTuThien/Controllers/NguoiDungController.cs
+++ DongGopTuThien/Controllers/NguoiDungController.cs
@@ -166,5 +166,74 @@ namespace DongGopTuThien.Controllers
                 return UnprocessableEntity();
             }
         }
+
+        // api/NguoiDung/{id}/submitPaper
+        [HttpPut("{id}/submitPaper")]
+        public async Task<IActionResult> SubmitPaper(int id, IFormFile file)
+        {
+            if (file == null || file.Length == 0 || file.ContentType != "image/jpeg")
+                return BadRequest();
+
+            // Access user claims from JWT
+            var userIdClaim = User.FindFirst(ClaimTypes.Sub);
+            if (userIdClaim == null)
+            {
+                return Unauthorized();
+            }
+
+            var nguoiDung = await _context.NguoiDungs.FirstOrDefaultAsync(u => u.IdnguoiDung == int.Parse(userIdClaim.Value));
+
+            if (nguoiDung == null)
+            {
+                return Unauthorized();
+            }
+
+            // Convert the file to byte array
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+                nguoiDung.GiayPhep = memoryStream.ToArray();
+            }
+
+            // TAM THOI AUTO VERIFIED 
+            nguoiDung.TrangThai = TrangThai.XacThucGiayPhep;
+
+
+            _context.NguoiDungs.Update(nguoiDung);
+            await _context.SaveChangesAsync();
+
+            return Ok("Image uploaded successfully.");
+        }
+
+        [HttpGet("{id}/downloadPaper")]
+        public async Task<IActionResult> DownloadPaper(int id)
+        {
+            // Access user claims from JWT
+            var userIdClaim = User.FindFirst(ClaimTypes.Sub);
+            if (userIdClaim == null)
+            {
+                return Unauthorized();
+            }
+
+            var nguoiDungHienTai = await _context.NguoiDungs.FirstOrDefaultAsync(u => u.IdnguoiDung == int.Parse(userIdClaim.Value));
+
+            if (nguoiDungHienTai == null)
+            {
+                return Unauthorized();
+            }
+
+            var nguoiDung = await _context.NguoiDungs.FindAsync(id);
+            if (nguoiDung == null || nguoiDung.GiayPhep == null)
+                return NotFound("File not found.");
+
+            string contentType = "image/jpeg";
+            // Optionally, provide a file name
+            string fileName = $"giayto-{id}.jpg";
+
+
+            // Return the file as a response
+            return File(nguoiDung.GiayPhep, contentType, fileName);
+        }
+
     }
 }
